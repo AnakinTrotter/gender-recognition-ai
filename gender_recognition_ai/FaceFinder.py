1164afbe4f6f47ce6f461705cefde7c4362c8725
AnakinTrotter <anakin.trotter@gmail.com>
2020-08-09 02:03:55 -0700

    Added face encoder
    Copied and adjusted the facial features method to be able to return face encodings since those may be all we actually need.

--- gender_recognition_ai/FaceFinder.py
+++ gender_recognition_ai/FaceFinder.py
@@ -19,14 +19,44 @@ def get_facial_features(max_photos=-1):
         num_dirs = os.listdir(numbered)
         # Iterates through all the jpg files in each numbered directory
         for a in num_dirs:
-            if -1 < max_photos <= len(faces):
-                return faces
             image_path = os.path.join(numbered, a)
             # Tries to load an image from the image_path
             try:
                 image = fr.load_image_file(image_path)
                 facial_features = fr.face_landmarks(image)
-                faces.append(facial_features)
+                for b in facial_features:
+                    if -1 < max_photos <= len(faces):
+                        return faces
+                    faces.append(b)
+            except PIL.UnidentifiedImageError as e:
+                # If the image_path turns out to be invalid, closes the program
+                # If the image_path points to a .mat or .md file, just skip over it
+                if "mat" and "md" not in str(image_path).split("."):
+                    print(e)
+                    exit("Unidentified Image Format")
+    return faces
+
+
+# Navigates to the pictures folder then returns a list of all the face pixel data
+# The number of faces returned is not more than max_photos in most conditions
+# A value of -1, the default value, for max_photos gets faces from all the photos
+def get_face_encodings(max_photos=-1):
+    faces = []
+    # Iterates the folders 00 to 99 in the wiki directory
+    for directory in os.listdir(pics_folder):
+        numbered = os.path.join(pics_folder, directory)
+        num_dirs = os.listdir(numbered)
+        # Iterates through all the jpg files in each numbered directory
+        for a in num_dirs:
+            image_path = os.path.join(numbered, a)
+            # Tries to load an image from the image_path
+            try:
+                image = fr.load_image_file(image_path)
+                face_encoding = fr.face_encodings(image)
+                for b in face_encoding:
+                    if -1 < max_photos <= len(faces):
+                        return faces
+                    faces.append(b)
             except PIL.UnidentifiedImageError as e:
                 # If the image_path turns out to be invalid, closes the program
                 # If the image_path points to a .mat or .md file, just skip over it
--- gender_recognition_ai/Start.py
+++ gender_recognition_ai/Start.py
@@ -1,6 +1,6 @@
 from gender_recognition_ai import FaceFinder
 
 # test
-faces = FaceFinder.get_facial_features()
+faces = FaceFinder.get_face_encodings(1)
 for a in faces:
     print(a)
