0284a6c7f6b26922563ee08b6d18d3b1b5794642
AnakinTrotter <anakin.trotter@gmail.com>
2020-08-09 18:33:51 -0700

    Added clarity/options
    Added a README inside of gender_recognition_ai for a bit more clarity and also gave the users the option to not overwrite existing data.

---
+++ gender_recognition_ai/README.md
@@ -0,0 +1,1 @@
+Run Start.py to compile the data into a usable format.
--- gender_recognition_ai/Start.py
+++ gender_recognition_ai/Start.py
@@ -1,7 +1,25 @@
 from gender_recognition_ai import FaceFinder, DataHandler
 import sklearn
+import os
 from sklearn import neural_network
 from sklearn.utils import shuffle
+
+overwrite = False
+
+if "data.csv" in os.listdir():
+    print("\nA data.csv file was detected.")
+    print("Would you like to overwrite it? (y/n)")
+    should_prompt = True
+    while should_prompt:
+        response = input()
+        if response == "y":
+            overwrite = True
+            should_prompt = False
+        elif response == "n":
+            overwrite = False
+            should_prompt = False
+        else:
+            print("Please respond with 'y' for yes or 'n' for no.")
 
 print("\n")
 print("Please enter which folder to start on/end on.")
@@ -10,33 +28,35 @@ print("ex: [start, end)")
 print("It's like substring in Java.")
 print("\n")
 
-should_prompt = True
-start = -1
-end = -1
+if overwrite:
+    should_prompt = True
+    start = -1
+    end = -1
 
-while should_prompt:
-    try:
-        start = int(input("Starting folder: "))
-        end = int(input("Ending folder: "))
-    except ValueError:
-        print("Input must be an int between 0 and 100!\n")
-        continue
-    if -1 < start < end <= 100:
-        should_prompt = False
-    else:
-        print("Invalid input!\n")
+    while should_prompt:
+        try:
+            start = int(input("Starting folder: "))
+            end = int(input("Ending folder: "))
+        except ValueError:
+            print("Input must be an int between 0 and 100!\n")
+            continue
+        if -1 < start < end <= 100:
+            should_prompt = False
+        else:
+            print("Invalid input!\n")
 
-faces = FaceFinder.get_face_encodings(start_folder=start, end_folder=end)
-labels = FaceFinder.get_face_labels()
+    print("\nProcess started... this may take several hours.\n")
+    faces = FaceFinder.get_face_encodings(start_folder=start, end_folder=end)
+    labels = FaceFinder.get_face_labels()
 
-print("Genders: ", labels)
+    print("Corresponding genders: ", labels)
 
-print("\nData has been GOTTEN!")
-print("\nWriting data...")
-DataHandler.write_to_file(faces, labels)
-print("Data saved to data.csv")
+    print("\nData has been GOTTEN!")
+    print("\nWriting data...")
+    DataHandler.write_to_file(faces, labels)
+    print("Data saved to data.csv")
 
-print("Reading data...\n")
+print("\nReading data...\n")
 data = DataHandler.read_from_file()
 
 faces = DataHandler.format_face_data(data)
