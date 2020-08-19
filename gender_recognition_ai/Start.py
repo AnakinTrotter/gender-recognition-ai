585abfd240f7614a7d35e31295294ad2698eb38a
AnakinTrotter <anakin.trotter@gmail.com>
2020-08-19 13:22:36 -0700

    Model saved
    Finished loading the data and trained a model to 95+% accuracy.

--- gender_recognition_ai/Start.py
+++ gender_recognition_ai/Start.py
@@ -1,6 +1,8 @@
 from gender_recognition_ai import FaceFinder, DataHandler
 import sklearn
 import os
+import tensorflow as tf
+from tensorflow import keras
 from sklearn import neural_network
 from sklearn.utils import shuffle
 
@@ -74,3 +76,38 @@ train_images, test_images, train_labels, \
     test_labels = sklearn.model_selection.train_test_split(faces, labels, test_size=0.2)
 
 print("\nSuccess!\n")
+
+retrain = True
+
+if "model.h5" in os.listdir():
+    print("\nA model.h5 file was detected.")
+    print("Would you like to retrain the model? (y/n)")
+    should_prompt = True
+    while should_prompt:
+        response = input()
+        if response == "y":
+            retrain = True
+            should_prompt = False
+        elif response == "n":
+            retrain = False
+            should_prompt = False
+        else:
+            print("Please respond with 'y' for yes or 'n' for no.")
+
+if retrain:
+    layer1 = keras.layers.Dense(128)
+    layer2 = keras.layers.Dense(65, activation="relu")
+    layer3 = keras.layers.Dense(1, activation="sigmoid")
+
+    model = keras.Sequential([layer1, layer2, layer3])
+
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    model.fit(train_images, train_labels, epochs=6)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print("Tested acc: ", test_acc*100, "%")
+
+    model.save("model.h5")
+
+print("\nDone!\n")
